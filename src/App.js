3d6a0c6b4d15d3602c4663d5ad771f867f6856d3
Maymoona Al Balushi <maymoona.al.boloshi@gmail.com>
2021-02-17 12:21:06 +0400

    trigger cloud run

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           this is not good :( :(( super sad now
+    okey dokey cloud run passed
         </p>
         <a
           className="App-link"
